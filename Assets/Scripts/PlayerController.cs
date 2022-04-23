d888437fd71aae8c7259bfd323b8b062c5075781
Rom1BVT <102188122+Rom1BVT@users.noreply.github.com>
2022-04-23 17:09:59 +0200

    Add turret movement
    The turret follow the mouse into the screen
    
    Implemented in PlayerController.cs script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,6 +11,13 @@ public class PlayerController : MonoBehaviour
     private float zPositionMin = -5;
     private float rotationSpeed = 100;
 
+    private Camera cam;
+    public GameObject turret;
+
+    void Start()
+    {
+        cam = Camera.main;
+    }
 
     // Update is called once per frame
     void Update()
@@ -18,10 +25,13 @@ public class PlayerController : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
         float horizontalInput = Input.GetAxis("Horizontal");
         float yEulerRotation = transform.rotation.eulerAngles.y;
-        if (yEulerRotation > 180) { yEulerRotation -= 360; }
+        if (yEulerRotation > 180) { yEulerRotation -= 360; } //return the rotation between -180 and 180 
 
+        //add a translation to the player
         transform.Translate(Vector3.forward * verticalInput * speed * Time.deltaTime, Space.World);
         transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime, Space.World);
+
+        //avoid the player to quit the road
         if (transform.position.z < zPositionMin)
         {
             transform.position = new Vector3(transform.position.x, transform.position.y, zPositionMin);
@@ -41,7 +51,7 @@ public class PlayerController : MonoBehaviour
         }
 
 
-
+        // add a rotation when the player moves left or right
         if (horizontalInput > 0)
         {
             if (yEulerRotation < yRotationMax)
@@ -67,6 +77,22 @@ public class PlayerController : MonoBehaviour
                 transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
             }
         }
+
+        // add a rotation to the turret to follow cursor
+            
+        //get the mouse position in world space coordinates
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = GameObject.Find("Main Camera").transform.position.y;
+        Vector3 mouseWorldPos = cam.ScreenToWorldPoint(mousePosition);
+
+        //calculate the angle between the player position and the mouse position
+        Quaternion turretRotation = Quaternion.FromToRotation(Vector3.forward, transform.position - mouseWorldPos);
+
+        //apply this angle to the turret (reset the rotation and apply a rotate action) 
+        float yTurretAngle = turretRotation.eulerAngles.y - 180;
+        Debug.Log(yTurretAngle);
+        turret.transform.rotation = new Quaternion(0, 0, 0, 0);
+        turret.transform.Rotate(0, yTurretAngle, 0);
 
     }
 }
